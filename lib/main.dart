a299f8df43d3408081f35989afd42e6d0ca5dc82
onlyhappiness <onlyhappiness96@gmail.com>
2023-09-04 20:34:54 +0900

    :sparkles: Feat: .env 및 supabase 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,56 @@
-import 'package:do_it_supabase/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  var supabaseUrl = dotenv.env["SUPABASE_URL"];
+  var supabaseKey = dotenv.env["SUPABASE_ANON_KEY"];
 
   await Supabase.initialize(
-    url: 'YOUR_SUPABASE_URL',
-    anonKey: 'YOUR_SUPABASE_ANON_KEY',
+    url: supabaseUrl.toString(),
+    anonKey: supabaseKey.toString(),
   );
 
-  runApp(MaterialApp(
-    home: HomeScreen(),
-  ));
+  runApp(MaterialApp(home: HomeScreen()));
 }
 
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomeScreen> {
+  final _future = Supabase.instance.client
+      .from('test')
+      .select<List<Map<String, dynamic>>>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: _future,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final countries = snapshot.data!;
+          return ListView.builder(
+            itemCount: countries.length,
+            itemBuilder: ((context, index) {
+              final country = countries[index];
+              return ListTile(
+                title: Text(country['name']),
+              );
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
